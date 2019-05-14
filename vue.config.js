b4fe5a2f0be1111b39328ae13f5dba986e09ad4e
Duong Nguyen <dnguyen@bocasay.com>
2019-05-14 13:04:55 +0700

    test SEO

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const path = require('path');
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/vuejs-boilerplace/'
-        : '/'
-}
+    publicPath: '/',
+    outputDir: path.join(__dirname, 'dist'),
+    configureWebpack: {
+        plugins: [
+            new PrerenderSPAPlugin({
+                staticDir: path.join(__dirname, 'dist'),
+                outputDir: path.join(__dirname, 'dist'),
+                indexPath: path.join(__dirname, 'dist', 'index.html'),
+                routes: [
+                    '/',
+                    '/login',
+                    '/dashboard',
+                    '/about'
+                ],
+            }),
+        ],
+    },
+};
